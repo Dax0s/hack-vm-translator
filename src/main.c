d3bb046a9c151da882dd4e4a12938b2a6168b367
Dax0s <dev@dklimavicius.com>
2025-01-16 17:50:14 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "Parser.h"
+
+/**
+ * add
+ * sub
+ * neg
+ * eq
+ * gt
+ * lt
+ * and
+ * or
+ * not
+ * pop segment i
+ * push segment i
+ * push pointer 0/1 -> *SP = THIS/THAT, SP++
+ * [local, argument, this, that] -> [LCL, ARG, THIS, THAT]
+ */
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}
